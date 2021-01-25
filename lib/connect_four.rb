39062277ef2fd471f4b8cad4992e8d36ef9bcaf5
iKimon <{ID}+{username}@users.noreply.github.com>
2021-01-25 16:18:39 +0100

    Add run functionality

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,9 +1,19 @@
 require "connect_four/board"
+require "connect_four/input/place_chip"
 require "connect_four/output/terminal_printer"
 
 module ConnectFour
   def self.run
-    #"hello"
+    board = ConnectFour::Board.new
+    game_over = false
+    while !game_over
+      ConnectFour::Output::TerminalPrinter.print_board(board)
+      chosen_move = gets.chomp.to_i
+      chosen_move = ConnectFour::Input::PlaceChip.valid_move(chosen_move)
+      ConnectFour::Input::PlaceChip.place_chip(chosen_move - 1, board)
+      board.toggle_turn
+      puts `clear`
+    end
     true
   end
 end
