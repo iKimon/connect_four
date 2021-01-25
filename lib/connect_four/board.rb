faffc5151e026ad1b7977d77d43ca24aad5001ba
iKimon <{ID}+{username}@users.noreply.github.com>
2021-01-25 16:22:44 +0100

    Add turn system on board creation

---
+++ lib/connect_four/board.rb
@@ -0,0 +1,25 @@
+module ConnectFour
+  class Board
+    attr_reader :board
+    attr_reader :p1_turn
+    attr_reader :row_full
+
+    def initialize
+      @board = Array.new(8) { Array.new(8, ".") }
+      @row_full = Array.new(8, false)
+      @p1_turn = true
+    end
+
+    def toggle_turn
+      @p1_turn = !@p1_turn
+    end
+
+    def place_chip(chosen_move)
+      ConnectFour::Input::PlaceChip.place_chip(chosen_move - 1, board)
+    end
+
+    def print_board
+      ConnectFour::Output::TerminalPrinter.print_board(board)
+    end
+  end
+end
